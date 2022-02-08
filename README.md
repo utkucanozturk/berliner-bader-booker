93e159116e9ba5ffb5fd747114cccfef73d98c30
utkucanozturk <utkucanozturk@gmail.com>
2022-02-08 13:39:54 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# berliner-bader-booker
+## Booking Alerts for Berliner Bäder
+
+Script that automaticly checks the available tickets for the swimming halls in Berlin.
+
+### Swimming Halls
+
+It can be used for the swimming halls listed in [this website](https://pretix.eu/Baeder/).
+
+### Input Values
+
+4 input values are required for program to run:
+
+* Swimming hall id. It can be found using [this website}(https://pretix.eu/Baeder/). When you clicked on the swimming hall you are interested in, you will be redirected to the URL for that pool. If you check the URL address you will see that it ends with a number such as `49` for Stadtbad Neukölln. That number should be inputted as hall id in the application.
+
+* Date you want to make a search. It should be in the format of `YYYY-MM-DD`.
+
+* Time you want to make a search. It should be in the `HH:MM` 24h format.
+
+* Search duration in minutes.
+
+### How it works?
+
+The program checks the specified swimming hall's ticket website every minute whether if there are any empty tickets for the inputted date and time or not. If there are empty spots, you will hear a victory sound and program will stop running.
---
+++ berliner_baeder_booker.py
@@ -0,0 +1,49 @@
+import time
+from beepy import beep
+import requests
+from bs4 import BeautifulSoup
+
+baeder_id = input('Enter hall id as number e.g. 49: ')
+data_date_ = input('Enter date in YYYY-MM-DD format: ')
+data_time_ = input('Enter time in HH:MM 24h format: ')
+search_len = input('Enter search duration in minutes: ')
+search_len = int(search_len)
+
+base_url = "https://pretix.eu/Baeder/"
+
+URL = base_url + str(baeder_id) + "/"
+
+
+for i in range(search_len):
+    page = requests.get(URL, verify='C:/Users/ucn/Desktop/pretix-eu-chain.pem')
+
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    results = soup.find(id='subevent-list')
+    data_elements = results.find_all("td", class_ = 'day has-events')
+
+    for elem in data_elements:
+        if elem.find("time", {"datetime": data_date_}):
+            for event in elem.find_all('li'):
+                if event.find("time", {"datetime": data_time_}):
+                    time_event = event
+                    break
+        
+    if time_event.find('a', 'event over'):
+        print('This event is over! Please enter another date/time.')
+        break
+    elif (time_event.find('a', 'event soldout')) or (time_event.find('a', 'event soon')) or (time_event.find('a', 'event reserved')):
+        print('Not available!')
+    else:
+        print('Found one, hurry!')
+        beep(sound='success')
+        break
+    
+    # wait for 1 minute
+    print("Let's wait one minute and try again!")
+    
+    if i == search_len -1:
+        print("Search finished! We couldn't find any empty slots during the search period. You can try to search longer.")
+        break
+
+    time.sleep(60)
